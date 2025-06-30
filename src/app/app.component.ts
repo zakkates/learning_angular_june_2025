ce91e97962a5b9bfe34598d74b0912785839d7f8
Zakery Kates <zakkates@simple-edge.com>
2025-06-30 15:48:01 -0500

    Including Header Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
 })
 export class AppComponent {
   title = 'learning-angular-june-2025';
